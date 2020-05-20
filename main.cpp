53ea4bed205a1372073f9bf6f94a41aaf879426c
Mericustar <1009699847@qq.com>
2020-05-21 00:40:22 +0800

    init project, add basic architecture

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <regex>
+
+void Regex();
+void Usage();
+
+int main(int argc, char const *argv[])
+{
+    // Regex();
+    Usage();
+
+    return 0;
+}
+
+void Regex()
+{
+    std::string testTemplateString = "TEST: $CONTENT";
+    std::string testFormatString = "This is an error: $CONTENT";
+    std::string testErrorString = "test error string";
+
+    const std::string CONTENT_EXPRESSION = "\\$CONTENT";
+    std::regex contentRegex(CONTENT_EXPRESSION);
+
+    std::string replaceErrorContent = regex_replace(testFormatString, contentRegex, testErrorString);
+    std::cout << replaceErrorContent << std::endl;
+
+    std::string replaceContent = regex_replace(testTemplateString, contentRegex, replaceErrorContent);
+    std::cout << replaceContent << std::endl;
+}
+
+void TestFormatOutput(std::string & formatContent, std::stringstream &contentStringStream)
+{
+    std::string testTemplateString = "TEST: %v";
+    const std::string REPLACE_EXPRESSION = "%v";
+    std::regex replaceRegex(REPLACE_EXPRESSION);
+    std::string content;
+    while (contentStringStream >> content)
+    {
+        formatContent = regex_replace(formatContent, replaceRegex, content, std::regex_constants::format_first_only);
+    }
+    formatContent = std::regex_replace(testTemplateString, replaceRegex, formatContent, std::regex_constants::format_first_only);
+}
+
+void Usage()
+{
+    int index = 10;
+    int value = 5;
+    std::string ouputContentFormat = "Current index = %v, and value = %v";
+    std::stringstream outputContentValueStringStream;
+    outputContentValueStringStream << index << " " << value;
+
+    TestFormatOutput(ouputContentFormat, outputContentValueStringStream);
+
+    std::cout << "After TestFormatOutput, " << ouputContentFormat << std::endl;
+}
