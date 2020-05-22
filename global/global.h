853313df45f23216907788eeac600ecd09243614
Mericustar <1009699847@qq.com>
2020-05-23 00:51:08 +0800

    add definition of utility.h, complete part of functions

---
+++ global/global.h
@@ -0,0 +1,25 @@
+#include <string>
+#include <regex>
+
+namespace global
+{
+    // 内部常量
+
+    // UI 常量
+    const std::string LogMarkTest = "TEST";
+    const std::string LogMarkNote = "Note";
+    const std::string LogMarkError = "Error";
+    const std::string LogMarkWarn = "Warn";
+    const std::string REPLACE_EXPRESSION = "%v";
+    const std::regex replaceRegex(REPLACE_EXPRESSION);
+
+    // 配置常量
+    const std::string SyntaxGo = "go";
+    const std::string SyntaxCSV = "csv";
+    const std::string TemplateKeywordExpression = "TKExpression";
+
+    // 外部常量
+    const std::string ConfigProjectPathKey = "path";
+    const std::string ConfigProjectSyntaxKey = "syntax";
+    const std::string ConfigConvertCsvHeadKey = "csv_head";
+} // namespace global
